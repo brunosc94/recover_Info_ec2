dff6173e5fe5be545b426751f3497eb854fb9209
bruno <bruno.cruz@gft.com.br>
2021-08-14 17:39:11 -0300

    Remoção de arquivo de teste e implementação do script

--- program.py
+++ program.py
@@ -1,7 +1,17 @@
 import boto3
+from mypy_boto3_ec2.service_resource import Vpc
 
-ec2 = boto3.client('ec2')
+ec2 = boto3.resource('ec2')
+sg = ec2.security_groups('GroupName')
+vpc = ec2.vpcs()
 
-response = ec2.describe_security_groups()
+i = 1
+for instance in ec2.instances.all():
+    if instance.state['Name'] == 'running':
 
-print(response)
+        print(
+            "Instância " + str(i) + "\nId: {0}\nSecurity Group: {1}\nVpc: {2}\nInstance Type: {3}\nState: {4}\n".format(
+                instance.id, instance.security_groups, instance.vpc, instance.instance_type, instance.state
+            )
+        )
+    i += 1
