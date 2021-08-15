87cab85be8c4826133fe110411a8fd47a5173705
bruno <bruno.cruz@gft.com.br>
2021-08-15 14:57:02 -0300

    Feito remoção de variáveis sem uso do script

--- program.py
+++ program.py
@@ -1,17 +1,13 @@
 import boto3
-from mypy_boto3_ec2.service_resource import Vpc
 
 ec2 = boto3.resource('ec2')
-sg = ec2.security_groups('GroupName')
-vpc = ec2.vpcs()
 
 i = 1
 for instance in ec2.instances.all():
     if instance.state['Name'] == 'running':
-
         print(
-            "Instância " + str(i) + "\nId: {0}\nSecurity Group: {1}\nVpc: {2}\nInstance Type: {3}\nState: {4}\n".format(
-                instance.id, instance.security_groups, instance.vpc, instance.instance_type, instance.state
+            "Instância " + str(i) + "\nId: {0}\nInstance Type: {1}\nSecurity Group: {2}\nVpc: {3}\nSubnets: {4}\nState: {5}\n".format(
+                instance.id, instance.instance_type, instance.security_groups, instance.vpc, instance.subnet, instance.state
             )
         )
     i += 1
